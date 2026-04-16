d050fcc6233bafd59b2176e6ad2f2848cf655dc4
Patrick-Santana <zonaabissal2016@gmail.com>
2026-04-16 02:01:20 -0300

    JPA Repository, depedence injection, database seeding

--- src/main/java/org/example/webservice/entitites/User.java
+++ src/main/java/org/example/webservice/entitites/User.java
@@ -1,5 +1,6 @@
 package org.example.webservice.entitites;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,11 +10,15 @@ import java.util.Objects;
 
 @Setter
 @Getter
+@Entity
+@Table(name = "tb_user")
 public class User  implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
-    private  long id;
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String email;
     private String phone;
@@ -21,7 +26,7 @@ public class User  implements Serializable {
 
     public User() {
     }
-    public User(long id,String name, String email, String phone, String password) {
+    public User(Long id,String name, String email, String phone, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
---
+++ src/main/java/org/example/webservice/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package org.example.webservice.repositories;
+
+import org.example.webservice.entitites.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
